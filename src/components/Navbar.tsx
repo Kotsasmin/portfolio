54645ad2392e07541de969b27550a1e5c8fb0f5e
Kotsasmin <kotsasmin@gmail.com>
2024-09-10 14:38:46 +0300

    A few changes Now the contact info is displayed correctly on small devices the navbar fade effect got removed and is now on top a few more changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,17 @@
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faProjectDiagram, faEnvelope, faCartShopping} from '@fortawesome/free-solid-svg-icons';
+import { faHome, faUser, faProjectDiagram, faEnvelope, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul>
-        <li><Link to="/"><FontAwesomeIcon icon={faHome} /> Home</Link></li>
-        <li><Link to="/about"><FontAwesomeIcon icon={faUser} /> About</Link></li>
-        <li><Link to="/projects"><FontAwesomeIcon icon={faProjectDiagram} /> Projects</Link></li>
-        <li><Link to="/contact"><FontAwesomeIcon icon={faEnvelope} /> Contact</Link></li>
-        <li><Link to="/order"><FontAwesomeIcon icon={faCartShopping} /> Order</Link></li>
+        <li><Link to="/"><FontAwesomeIcon icon={faHome} /><span> Home</span></Link></li>
+        <li><Link to="/about"><FontAwesomeIcon icon={faUser} /><span> About</span></Link></li>
+        <li><Link to="/projects"><FontAwesomeIcon icon={faProjectDiagram} /><span> Projects</span></Link></li>
+        <li><Link to="/contact"><FontAwesomeIcon icon={faEnvelope} /><span> Contact</span></Link></li>
+        <li><Link to="/order"><FontAwesomeIcon icon={faCartShopping} /><span> Order</span></Link></li>
       </ul>
     </nav>
   );
