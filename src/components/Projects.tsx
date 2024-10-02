174da73a43142a97f5c4404f8dd3900ddabb243d
Kotsasmin <kotsasmin@gmail.com>
2024-10-02 14:56:45 +0300

    Some changes in projects page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,8 +3,10 @@ import '../styles/Projects.css'; import { FontAwesomeIcon } from '@fortawesome/r
       <FontAwesomeIcon icon={faProjectDiagram} /> Projects
     </h2>
     <p>Here are some of my most notable projects:</p>
-    <h4 className="soon-available">The projects page is under heavy development. Github and download links will be available in the future.</h4>
+    <h4 className="soon-available">The projects page is under heavy development. GitHub and download links will be available in the future.</h4>
+<h4 className="soon-available">Many more projects are on their way, it is only a matter of time before they are uploaded.</h4>
     <div className="project-list">
+{/*
       <div className="project-item">
         <h3>Chat App</h3>
         <p>A real-time web and desktop chat application with user authentication, channel creation, and lightweight database integration.</p>
@@ -28,8 +30,9 @@ import '../styles/Projects.css'; import { FontAwesomeIcon } from '@fortawesome/r
           <li>Node.js</li>
         </ul>
         <p>Project link coming soon</p>
-      </div>
+      </div>*/}
       <div className="project-item">
+      <a href="https://kotsasmindashboard.netlify.app/" target="_blank" rel="noopener noreferrer" className="dashboard-link">
         <h3>Dashboard</h3>
         <p>An all-in-one modern-looking dashboard for note-taking, web searching, Pomodoro timer, weather forecast, and website shortcuts.</p>
         <ul className="tech-list">
@@ -37,11 +40,10 @@ import '../styles/Projects.css'; import { FontAwesomeIcon } from '@fortawesome/r
           <li>Tailwind CSS</li>
           <li>Node.js</li>
         </ul>
-        <a href="https://kotsasmindashboard.netlify.app/" target="_blank" rel="noopener noreferrer">
-          <button>Take a look</button>
         </a>
       </div>
       <div className="project-item">
+      <a href="http://54.234.50.111" target="_blank" rel="noopener noreferrer" className="dashboard-link">
         <h3>Personal Website</h3>
         <p>A personal portfolio website showcasing my skills, projects, and contact information.</p>
         <ul className="tech-list">
@@ -49,10 +51,9 @@ import '../styles/Projects.css'; import { FontAwesomeIcon } from '@fortawesome/r
           <li>Vite</li>
           <li>Nginx</li>
         </ul>
-        <a href="http://kotsasmin.ddns.net:8080" target="_blank" rel="noopener noreferrer">
-          <button>You are here XD</button>
         </a>
       </div>
+      {/*
       <div className="project-item">
         <h3>HomeLab Automation</h3>
         <p>A fully automated HomeLab system that can scale from a single Raspberry Pi to a large computing setup. There will be installation of CasaOS, proxies, containers, DNS servers, file hosting, and a central management dashboard.</p>
@@ -66,6 +67,6 @@ import '../styles/Projects.css'; import { FontAwesomeIcon } from '@fortawesome/r
           <li>Firewall</li>
         </ul>
         <p>Project link coming soon</p>
-      </div>
+      </div>*/}
     </div>
   </div> ); } export default Projects;
