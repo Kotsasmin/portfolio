8d8c1435be39614c54880b00e7462b1e99682b63
Kotsasmin <kotsasmin@gmail.com>
2024-11-05 11:52:07 +0200

    Changed slogan text

--- src/components/Slogan.tsx
+++ src/components/Slogan.tsx
@@ -4,7 +4,7 @@ import '../styles/Slogan.css';
 const Slogan: React.FC = () => {
   return (
     <div className="slogan-container">
-      <h1 className="slogan-text">🚀 Innovative full-stack solutions tailored to bring your vision to life.</h1>
+      <h1 className="slogan-text">🚀 Crafting full-stack solutions that bring ideas to life.</h1>
     </div>
   );
 };
