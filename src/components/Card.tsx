43528feada640e9294997d837c22ddcf8dbbfb30
Kotsasmin <kotsasmin@gmail.com>
2025-05-31 14:07:13 +0300

    changed age/added arch

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import '../styles/Card.css';
 function Card() {
     return (
 <div className="dos">
-    This is a profile picture. All craftsmanship is of the highest quality. It is encircled with bands of digital code. On the item is an image of Kotsasmin, an 18-year-old web developer from Greece. Kotsasmin is surrounded by the glowing light of the screen. His mind menaces with the pursuit of knowledge and the art of technology. The picture is adorned with symbols of creativity and growth. His spirit burns with passion for the digital realm, ever seeking new challenges and mastery.
+    This is a profile picture. All craftsmanship is of the highest quality. It is encircled with bands of digital code. On the item is an image of Kotsasmin, a 19-year-old web developer from Greece. Kotsasmin is surrounded by the glowing light of the screen. His mind menaces with the pursuit of knowledge and the art of technology. The picture is adorned with symbols of creativity and growth. His spirit burns with passion for the digital realm, ever seeking new challenges and mastery.
 </div>
     );
   }
